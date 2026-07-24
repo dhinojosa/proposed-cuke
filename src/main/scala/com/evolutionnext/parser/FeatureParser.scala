2988d912e415f58b9ef4084f3b7cd74d0fd6618c
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2026-07-24 17:35:45 -0600

    Refactor `FeatureParser` for improved readability and update parsing logic to return `Either`

--- src/main/scala/com/evolutionnext/parser/FeatureParser.scala
+++ src/main/scala/com/evolutionnext/parser/FeatureParser.scala
@@ -5,114 +5,105 @@ import com.evolutionnext.gherkin.*
 
 object FeatureParser:
 
-    private val newline: P[Unit] =
-        P.string("\r\n").void.orElse(P.char('\n').void)
+  private val newline: P[Unit] =
+    P.string("\r\n").void.orElse(P.char('\n').void)
 
-    private val spaces0: P0[Unit] =
-        P.charIn(" \t").rep0.void
+  private val spaces0: P0[Unit] =
+    P.charIn(" \t").rep0.void
 
-    private val nonNewline: Char => Boolean =
-        ch => ch != '\n' && ch != '\r'
+  private val nonNewline: Char => Boolean =
+    ch => ch != '\n' && ch != '\r'
 
-    private val text: P[String] =
-        (P.charWhere(nonNewline) ~ P.charsWhile0(nonNewline)).map {
-            case (head, tail) => s"$head$tail"
-        }
+  private val text: P[String] =
+    (P.charWhere(nonNewline) ~ P.charsWhile0(nonNewline)).map {
+      case (head, tail) => s"$head$tail"
+    }
 
-    private val featureHeader: P[String] =
-        (P.string("Feature:") *> spaces0 *> text <* newline.?).map(_.trim)
+  private val featureHeader: P[String] =
+    (P.string("Feature:") *> spaces0 *> text <* newline.?).map(_.trim)
 
-    private val scenarioHeader: P[String] =
-        (P.string("Scenario:") *> spaces0 *> text <* newline.?).map(_.trim)
+  private val scenarioHeader: P[String] =
+    (P.string("Scenario:") *> spaces0 *> text <* newline.?).map(_.trim)
 
-    private def stepLine(prefix: String, keyword: StepKeyword): P[Step] =
-        (P.string(prefix) *> P.char(' ') *> text <* newline.?).map { stepText =>
-            Step(keyword, stepText.trim)
-        }
+  private def stepLine(prefix: String, keyword: StepKeyword): P[Step] =
+    (P.string(prefix) *> P.char(' ') *> text <* newline.?).map { stepText =>
+      Step(keyword, stepText.trim)
+    }
 
-    private val step: P[Step] =
-        stepLine("Given", StepKeyword.Given)
-            .backtrack
-            .orElse(stepLine("When", StepKeyword.When).backtrack)
-            .orElse(stepLine("Then", StepKeyword.Then).backtrack)
-            .orElse(stepLine("And", StepKeyword.And).backtrack)
-            .orElse(stepLine("But", StepKeyword.But))
+  private val step: P[Step] =
+    stepLine("Given", StepKeyword.Given)
+      .backtrack
+      .orElse(stepLine("When", StepKeyword.When).backtrack)
+      .orElse(stepLine("Then", StepKeyword.Then).backtrack)
+      .orElse(stepLine("And", StepKeyword.And).backtrack)
+      .orElse(stepLine("But", StepKeyword.But))
 
-    private val tagName: P[String] =
-        (P.charWhere(ch => !ch.isWhitespace && ch != '@') ~
-            P.charsWhile0(ch => !ch.isWhitespace && ch != '@')).map {
-            case (head, tail) => s"$head$tail"
-        }
+  private val tagName: P[String] =
+    (P.charWhere(ch => !ch.isWhitespace && ch != '@') ~
+      P.charsWhile0(ch => !ch.isWhitespace && ch != '@')).map {
+      case (head, tail) => s"$head$tail"
+    }
 
-    private val tag: P[Tag] =
-        (P.char('@') *> tagName).map(Tag.apply)
+  private val tag: P[Tag] =
+    (P.char('@') *> tagName).map(Tag.apply)
 
-    private val tagLine: P[List[Tag]] =
-        (tag.repSep(P.char(' ')) <* newline.?).map(_.toList)
+  private val tagLine: P[List[Tag]] =
+    (tag.repSep(P.char(' ')) <* newline.?).map(_.toList)
 
-    private val tags: P0[List[Tag]] =
-        tagLine.rep0.map(_.flatten)
+  private val tags: P0[List[Tag]] =
+    tagLine.rep0.map(_.flatten)
 
-    private val taggedScenario: P[Scenario] =
-        (tagLine.rep ~ scenarioHeader ~ step.rep).map {
-            case ((tagLines, name), steps) =>
-                Scenario(
-                    tags = tagLines.toList.flatten,
-                    name = name,
-                    steps = steps.toList
-                )
-        }
+  private val taggedScenario: P[Scenario] =
+    (tagLine.rep ~ scenarioHeader ~ step.rep).map {
+      case ((tagLines, name), steps) =>
+        Scenario(
+          tags = tagLines.toList.flatten,
+          name = name,
+          steps = steps.toList
+        )
+    }
 
-    private val untaggedScenario: P[Scenario] =
-        (scenarioHeader ~ step.rep).map { case (name, steps) =>
-            Scenario(
-                tags = Nil,
-                name = name,
-                steps = steps.toList
-            )
-        }
+  private val untaggedScenario: P[Scenario] =
+    (scenarioHeader ~ step.rep).map {
+      case (name, steps) =>
+        Scenario(
+          tags = Nil,
+          name = name,
+          steps = steps.toList
+        )
+    }
 
-    private val scenario: P[Scenario] =
-        taggedScenario.backtrack.orElse(untaggedScenario)
+  private val scenario: P[Scenario] =
+    taggedScenario.backtrack.orElse(untaggedScenario)
 
+  private val featureWithTags: P[Feature] =
+    (tagLine.rep ~ featureHeader ~ scenario.rep).map {
+      case ((tagLines, name), scenarios) =>
+        Feature(
+          tags = tagLines.toList.flatten,
+          name = name,
+          background = None,
+          description = Nil,
+          scenarios = scenarios.toList
+        )
+    }
 
+  private val featureWithoutTags: P[Feature] =
+    (featureHeader ~ scenario.rep).map {
+      case (name, scenarios) =>
+        Feature(
+          tags = Nil,
+          name = name,
+          background = None,
+          description = Nil,
+          scenarios = scenarios.toList
+        )
+    }
 
-    private val featureWithTags: P[Feature] =
-        (tagLine.rep ~ featureHeader ~ scenario.rep).map {
-            case ((tagLines, name), scenarios) =>
-                Feature(
-                    tags = tagLines.toList.flatten,
-                    name = name,
-                    background = None,
-                    description = Nil,
-                    scenarios = scenarios.toList
-                )
-        }
+  private val feature: P[Feature] =
+    featureWithTags.backtrack.orElse(featureWithoutTags)
 
-    private val featureWithoutTags: P[Feature] =
-        (featureHeader ~ scenario.rep).map {
-            case (name, scenarios) =>
-                Feature(
-                    tags = Nil,
-                    name = name,
-                    background = None,
-                    description = Nil,
-                    scenarios = scenarios.toList
-                )
-        }
-
-    private val feature: P[Feature] =
-            featureWithTags.backtrack.orElse(featureWithoutTags)
-
-    def parse(content: String): Feature =
-        val normalized =
-            content
-                .linesIterator
-                .map(_.trim)
-                .filter(_.nonEmpty)
-                .mkString("\n")
-
-        feature.parseAll(normalized) match
-            case Right(value) => value
-            case Left(error) =>
-                throw new IllegalArgumentException(s"Could not parse feature: $error")
+  def parse(content: String): Either[P.Error, Feature] =
+    val normalized =
+      content.linesIterator.map(_.trim).filter(_.nonEmpty).mkString("\n")
+    feature.parseAll(normalized)
--- src/test/scala/com/evolutionnext/parser/FeatureParserSuite.scala
+++ src/test/scala/com/evolutionnext/parser/FeatureParserSuite.scala
@@ -2,170 +2,210 @@ package com.evolutionnext.parser
 
 import cats.effect.*
 import cats.effect.unsafe.implicits.global
+import cats.parse.Parser
 import com.evolutionnext.gherkin.{Feature, Tag}
+import munit.Clue.generate
 
 import scala.io.Source
 
 class FeatureParserSuite extends munit.FunSuite {
-  test("canary test") {
-    assert(true)
-  }
-  test("read in 000-basic-scenario.feature content") {
-    val io: IO[String] = {
-      Resource
-        .make(IO(Source.fromResource("000-basic-scenario.feature")))(source =>
-          IO(source.close()))
-        .use(source => IO(source.mkString))
+    test("canary test") {
+        assert(true)
+    }
+    test("read in 000-basic-scenario.feature content") {
+        val io: IO[String] = {
+            Resource
+                .make(IO(Source.fromResource("000-basic-scenario.feature")))(source =>
+                    IO(source.close()))
+                .use(source => IO(source.mkString))
+        }
+
+        val content = io.unsafeRunSync()
+        assert(content.nonEmpty)
+        assert(content.contains("Feature: Basic arithmetic"))
+    }
+    test("read in 000-basic-scenario.feature") {
+        val io: IO[String] = {
+            Resource
+                .make(IO(Source.fromResource("000-basic-scenario.feature")))(source =>
+                    IO(source.close()))
+                .use(source => IO(source.mkString))
+        }
+
+        val content = io.unsafeRunSync()
+        val eitherFeature: Either[Parser.Error, Feature] = FeatureParser.parse(content)
+        val feature = eitherFeature match {
+            case Right(feature) => feature
+            case Left(error) => fail(s"could not parse feature: $error")
+        }
+
+        assertEquals(feature.name, "Basic arithmetic")
+        assertEquals(feature.tags, Nil)
+        assertEquals(feature.description, Nil)
+        assertEquals(feature.scenarios.length, 1)
+
+        val scenario = feature.scenarios.head
+        assertEquals(scenario.name, "Add two numbers")
+        assertEquals(scenario.tags, Nil)
+
+        assertEquals(
+            scenario.steps,
+            List(
+                com
+                    .evolutionnext
+                    .gherkin
+                    .Step(com.evolutionnext.gherkin.StepKeyword.Given, "the number 2"),
+                com
+                    .evolutionnext
+                    .gherkin
+                    .Step(com.evolutionnext.gherkin.StepKeyword.And, "the number 3"),
+                com
+                    .evolutionnext
+                    .gherkin
+                    .Step(com.evolutionnext.gherkin.StepKeyword.When, "I add the numbers"),
+                com
+                    .evolutionnext
+                    .gherkin
+                    .Step(com.evolutionnext.gherkin.StepKeyword.Then, "the result should be 5")
+            )
+        )
     }
 
-    val content = io.unsafeRunSync()
-    assert(content.nonEmpty)
-    assert(content.contains("Feature: Basic arithmetic"))
-  }
-  test("read in 000-basic-scenario.feature") {
-    val io: IO[String] = {
-      Resource
-        .make(IO(Source.fromResource("000-basic-scenario.feature")))(source =>
-          IO(source.close()))
-        .use(source => IO(source.mkString))
+    test("basic parse with annotation") {
+        val string =
+            """@Foo
+              |Feature: Basic arithmetic
+              |
+              |    Scenario: Add two numbers
+              |        Given the number 2
+              |        And the number 3
+              |        When I add the numbers
+              |        Then the result should be 5
+              |""".stripMargin
+
+        val eitherFeature: Either[Parser.Error, Feature] = FeatureParser.parse(string)
+        val feature = eitherFeature match {
+            case Right(feature) => feature
+            case Left(error) => fail(s"could not parse feature: $error")
+        }
+        assertEquals(feature.tags, List(Tag("Foo")))
+        assertEquals(feature.name, "Basic arithmetic")
+        assert(feature.scenarios.nonEmpty)
+        assertEquals(feature.scenarios.head.name, "Add two numbers")
+        assertEquals(feature.scenarios.head.steps.size, 4)
     }
 
-    val content = io.unsafeRunSync()
-    val feature: Feature = FeatureParser.parse(content)
+    test("basic parse with two annotations") {
+        val string =
+            """@Foo @Bar
+              |Feature: Basic arithmetic
+              |
+              |    Scenario: Add two numbers
+              |        Given the number 2
+              |        And the number 3
+              |        When I add the numbers
+              |        Then the result should be 5
+              |""".stripMargin
 
-    assertEquals(feature.name, "Basic arithmetic")
-    assertEquals(feature.tags, Nil)
-    assertEquals(feature.description, Nil)
-    assertEquals(feature.scenarios.length, 1)
+        val eitherFeature: Either[Parser.Error, Feature] = FeatureParser.parse(string)
+        val feature = eitherFeature match {
+            case Right(feature) => feature
+            case Left(error) => fail(s"could not parse feature: $error")
+        }
 
-    val scenario = feature.scenarios.head
-    assertEquals(scenario.name, "Add two numbers")
-    assertEquals(scenario.tags, Nil)
+        assertEquals(feature.tags, List(Tag("Foo"), Tag("Bar")))
+        assertEquals(feature.name, "Basic arithmetic")
+        assert(feature.scenarios.nonEmpty)
+        assertEquals(feature.scenarios.head.name, "Add two numbers")
+        assertEquals(feature.scenarios.head.steps.size, 4)
+    }
 
-    assertEquals(
-      scenario.steps,
-      List(
-        com
-          .evolutionnext
-          .gherkin
-          .Step(com.evolutionnext.gherkin.StepKeyword.Given, "the number 2"),
-        com
-          .evolutionnext
-          .gherkin
-          .Step(com.evolutionnext.gherkin.StepKeyword.And, "the number 3"),
-        com
-          .evolutionnext
-          .gherkin
-          .Step(com.evolutionnext.gherkin.StepKeyword.When, "I add the numbers"),
-        com
-          .evolutionnext
-          .gherkin
-          .Step(com.evolutionnext.gherkin.StepKeyword.Then, "the result should be 5")
-      )
-    )
-  }
+    test("basic parse with two annotations and two scenarios") {
+        val string =
+            """@Foo @Bar
+              |Feature: Basic arithmetic
+              |
+              |    Scenario: Add two numbers
+              |        Given the number 2
+              |        And the number 3
+              |        When I add the numbers
+              |        Then the result should be 5
+              |
+              |    Scenario: Add two different numbers
+              |        Given the number 5
+              |        And the number 7
+              |        When I add the numbers
+              |        Then the result should be 12
+              |""".stripMargin
 
-  test("basic parse with annotation") {
-    val string =
-      """@Foo
-        |Feature: Basic arithmetic
-        |
-        |    Scenario: Add two numbers
-        |        Given the number 2
-        |        And the number 3
-        |        When I add the numbers
-        |        Then the result should be 5
-        |""".stripMargin
+        val eitherFeature: Either[Parser.Error, Feature] = FeatureParser.parse(string)
+        val feature = eitherFeature match {
+            case Right(feature) => feature
+            case Left(error) => fail(s"could not parse feature: $error")
+        }
+        assertEquals(feature.tags, List(Tag("Foo"), Tag("Bar")))
+        assertEquals(feature.name, "Basic arithmetic")
+        assert(feature.scenarios.nonEmpty)
+        assertEquals(feature.scenarios.length, 2)
+    }
 
-    val feature = FeatureParser.parse(string)
-    assertEquals(feature.tags, List(Tag("Foo")))
-    assertEquals(feature.name, "Basic arithmetic")
-    assert(feature.scenarios.nonEmpty)
-    assertEquals(feature.scenarios.head.name, "Add two numbers")
-    assertEquals(feature.scenarios.head.steps.size, 4)
-  }
+    test("basic parse with two annotations and two scenarios, tags on the first") {
+        val string =
+            """@Foo @Bar
+              |Feature: Basic arithmetic
+              |
+              |    @Baz
+              |    Scenario: Add two numbers
+              |        Given the number 2
+              |        And the number 3
+              |        When I add the numbers
+              |        Then the result should be 5
+              |
+              |    Scenario: Add two different numbers
+              |        Given the number 5
+              |        And the number 7
+              |        When I add the numbers
+              |        Then the result should be 12
+              |""".stripMargin
 
-  test("basic parse with two annoations") {
-    val string =
-      """@Foo @Bar
-        |Feature: Basic arithmetic
-        |
-        |    Scenario: Add two numbers
-        |        Given the number 2
-        |        And the number 3
-        |        When I add the numbers
-        |        Then the result should be 5
-        |""".stripMargin
+        val eitherFeature: Either[Parser.Error, Feature] = FeatureParser.parse(string)
+        val feature = eitherFeature match {
+            case Right(feature) => feature
+            case Left(error) => fail(s"could not parse feature: $error")
+        }
+        assertEquals(feature.tags, List(Tag("Foo"), Tag("Bar")))
+        assertEquals(feature.name, "Basic arithmetic")
+        assert(feature.scenarios.nonEmpty)
+        assertEquals(feature.scenarios.length, 2)
+        assertEquals(feature.scenarios.head.tags, List(Tag("Baz")))
+    }
 
-    val feature = FeatureParser.parse(string)
-    assertEquals(feature.tags, List(Tag("Foo"), Tag("Bar")))
-    assertEquals(feature.name, "Basic arithmetic")
-    assert(feature.scenarios.nonEmpty)
-    assertEquals(feature.scenarios.head.name, "Add two numbers")
-    assertEquals(feature.scenarios.head.steps.size, 4)
-  }
+    test("parse 001-annotation-scenario.feature with a feature tag") {
+        val io: IO[String] =
+            Resource
+                .make(IO(Source.fromResource("001-annotation-scenario.feature")))(source =>
+                    IO(source.close()))
+                .use(source => IO(source.mkString))
 
-  test("basic parse with two annotations and two scenarios") {
-    val string =
-      """@Foo @Bar
-        |Feature: Basic arithmetic
-        |
-        |    Scenario: Add two numbers
-        |        Given the number 2
-        |        And the number 3
-        |        When I add the numbers
-        |        Then the result should be 5
-        |
-        |    Scenario: Add two different numbers
-        |        Given the number 5
-        |        And the number 7
-        |        When I add the numbers
-        |        Then the result should be 12
-        |""".stripMargin
+        val eitherFeature: Either[Parser.Error, Feature] =
+            io.map(FeatureParser.parse).unsafeRunSync()
+        val feature: Feature = eitherFeature match {
+            case Right(feature) => feature
+            case Left(error) => fail(s"could not parse feature: $error")
+        }
+        assertEquals(feature.tags, List(Tag("fast"), Tag("unit")))
+    }
 
-    val feature = FeatureParser.parse(string)
-    assertEquals(feature.tags, List(Tag("Foo"), Tag("Bar")))
-    assertEquals(feature.name, "Basic arithmetic")
-    assert(feature.scenarios.nonEmpty)
-    assertEquals(feature.scenarios.length, 2)
-  }
-
-  test("basic parse with two annotations and two scenarios, tags on the first") {
-    val string =
-      """@Foo @Bar
-        |Feature: Basic arithmetic
-        |
-        |    @Baz
-        |    Scenario: Add two numbers
-        |        Given the number 2
-        |        And the number 3
-        |        When I add the numbers
-        |        Then the result should be 5
-        |
-        |    Scenario: Add two different numbers
-        |        Given the number 5
-        |        And the number 7
-        |        When I add the numbers
-        |        Then the result should be 12
-        |""".stripMargin
-
-    val feature = FeatureParser.parse(string)
-    assertEquals(feature.tags, List(Tag("Foo"), Tag("Bar")))
-    assertEquals(feature.name, "Basic arithmetic")
-    assert(feature.scenarios.nonEmpty)
-    assertEquals(feature.scenarios.length, 2)
-    assertEquals(feature.scenarios.head.tags, List(Tag("Baz")))
-  }
-
-  test("parse 001-annotation-scenario.feature with a feature tag") {
-    val io: IO[String] =
-      Resource
-        .make(IO(Source.fromResource("001-annotation-scenario.feature")))(source =>
-          IO(source.close()))
-        .use(source => IO(source.mkString))
-
-    val feature =
-      io.map(FeatureParser.parse).unsafeRunSync()
-    assertEquals(feature.tags, List(Tag("fast"), Tag("unit")))
-  }
+    test("parse 002-data-tables.feature") {
+        val io: IO[String] =
+            Resource
+                .make(IO(Source.fromResource("002-data-tables.feature")))(source => IO(source.close()))
+                .use(source => IO(source.mkString))
+        val eitherFeature: Either[Parser.Error, Feature] =
+            io.map(FeatureParser.parse).unsafeRunSync()
+        val feature: Feature = eitherFeature match {
+            case Right(feature) => feature
+            case Left(error) => fail(s"could not parse feature: $error")
+        }
+    }
 }
